f47dca2036a52c8ab1fcffe661b93f6be17d6237
Tiana Johnson <tiana.marie013@gmail.com>
2019-01-31 14:50:29 -0600

    branch merged to local master

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {HashRouter as Router, Route, Link} from 'react-router-dom';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import Favorites from '../Favorites/Favorites'
 import Search from '../Search/Search'
 import './App.css';
@@ -9,27 +9,30 @@ class App extends Component {
   render() {
     return (
       <div>
-        <header>
-          <h1>Giphy Collectore</h1>
-          {/* <button className="serach-btn">Search</button>
-          <button className="favorite-btn">Favorites</button>     */}
-          <Router>
+
+        <Router>
           <div className="App-Paths">
-            <Link to="/">Search</Link>
-            <br/>
-            <Link to="/favorites">Favorites</Link>
-            <Route extact path ="/" component={Search} />
+            <header>
+              <h1>Giphy Collectore</h1>
+              {/* <button className="serach-btn">Search</button>
+          <button className="favorite-btn">Favorites</button>     */}
+              <Link to="/">Search</Link>
+              <br />
+              <Link to="/favorites">Favorites</Link>
+            </header>
+
+            <Route extact path="/" component={Search} />
             <Route extact path="/favorites" component={Favorites} />
           </div>
         </Router>
-          </header>    
-      <div>
-        {/* <h1>Giphy Search!</h1> */}
-      </div>
+
+        <div>
+          {/* <h1>Giphy Search!</h1> */}
+        </div>
       </div>
     );
   }
-  
+
 }
 
 export default App;
