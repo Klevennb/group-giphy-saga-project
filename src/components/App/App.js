72d4a21e920405ee284027a84b2a8ad224f46e11
Maxwell Todd <mtmaxtodd@gmail.com>
2019-01-31 13:09:56 -0600

    Added the base code to the project.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
-
-
+import {HashRouter as Router, Route, Link} from 'react-router-dom';
+import Favorites from '../Favorites/Favorites'
+import Search from '../Search/Search'
 class App extends Component {
 
   render() {
     return (
       <div>
+        <Router>
+          <div>
+            <Link to="/">Search</Link>
+            <Link to="/favorites">Favorites</Link>
+            <Route extact path ="/" component={Search} />
+            <Route extact path="/favorites" component={Favorites} />
+          </div>
+        </Router>
+      <div>
         <h1>Giphy Search!</h1>
+      </div>
       </div>
     );
   }
---
+++ src/components/Search/Search.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+// import axios from 'axios';
+class Search extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default connect()(Search);
