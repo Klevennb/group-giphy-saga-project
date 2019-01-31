3ec7da0de3a0b43b085351bd40e6c28231bdee76
Tiana Johnson <tiana.marie013@gmail.com>
2019-01-31 14:07:28 -0600

    header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,21 +2,31 @@ import React, { Component } from 'react';
 import {HashRouter as Router, Route, Link} from 'react-router-dom';
 import Favorites from '../Favorites/Favorites'
 import Search from '../Search/Search'
+import './App.css';
 class App extends Component {
+
 
   render() {
     return (
       <div>
+        <header>
+          <h1>Giphy Collectore</h1>
+          <button className="serach-btn">Search</button>
+          <button className="favorite-btn">Favorites</button>    
+          <br/>
+          <br/>
+          </header>    
         <Router>
           <div>
             <Link to="/">Search</Link>
+            <br/>
             <Link to="/favorites">Favorites</Link>
             <Route extact path ="/" component={Search} />
             <Route extact path="/favorites" component={Favorites} />
           </div>
         </Router>
       <div>
-        <h1>Giphy Search!</h1>
+        {/* <h1>Giphy Search!</h1> */}
       </div>
       </div>
     );
